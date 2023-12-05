23057823c4e3eef7da7a0cf69b159b02768aab38
Seong-Jun1525 <sjsj123455@naver.com>
2023-12-05 16:29:25 +0900

    product data 가져오기

---
+++ src/pages/HomePage/cardList/CardList.jsx
@@ -0,0 +1,19 @@
+import CardItem from "./card-item/CardItem"
+import { useEffect } from "react"
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux"
+import styles from "./CardList.module.scss"
+import { fetchProducts } from "../../../store/products/products.slice"
+const CardList = () => {
+    const dispatch = useAppDispatch()
+    const { products } = useAppSelector(state => state.productsSlice)
+    useEffect(() => {
+        dispatch(fetchProducts(styles))
+    }, [])
+    return (
+        <ul className={styles.card_list}>
+            {products.map(product => <CardItem key={product.id} item={product} />)}
+        </ul>
+    )
+}
+
+export default CardList
---
+++ src/store/products/products.slice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// createAsyncThunk: redux toolkit에서 비동기 요청을 할 때 사용
+export const fetchProducts = createAsyncThunk(
+    "products/fetchProducts",
+    async (thunkAPI) => {
+        console.log(thunkAPI)
+        try {
+            const response = await axios.get("https://fakestoreapi.com/products")
+            console.log("@@@@@ ", response)
+            return response.data
+        } catch (error) {
+            thunkAPI.rejectedWithValue("Error loading products")
+        }
+    }
+)
+
+const initialState = {
+    products: [],
+    isLoading: false,
+    error: ""
+}
+
+export const productsSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {
+
+    },
+
+    // reducer를 추가하면 Promise의 진행 상태에 따라서 리듀서를 실행할 수 있습니다.
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProducts.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.products = action.payload
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.isLoading = false
+                state.error = action.payload // Error loading products
+            })
+    }
+})
+
+export default productsSlice.reducer
