b68a97e077dadcde9429dd97caa2aabaf16f8c3b
Seong-Jun1525 <sjsj123455@naver.com>
2023-12-05 12:21:56 +0900

    create login&register page ui

---
+++ src/pages/LoginPage/sign-in/SignIn.jsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+    return (
+    <Form
+        title={"로그인"}
+    />
+    )
+}
+
+export default SignIn
--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {Link} from "react-router-dom"
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className="page">
+      <div className="form_container">
+        <h1>회원가입</h1>
+        <SignUp />
+        <p>
+          이미 계정이 없습니다?<Link to={"/login"}>가입하기</Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
