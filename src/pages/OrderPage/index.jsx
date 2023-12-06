1fa169340c4450a859fc1594eb7308aa21441994
Seong-Jun1525 <sjsj123455@naver.com>
2023-12-06 16:03:01 +0900

    create OrderList component

--- src/pages/OrderPage/index.jsx
+++ src/pages/OrderPage/index.jsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../../hooks/useAuth"
+import OrdersList from "./orders-list/OrdersList"
 
 const OrderPage = () => {
   const {isAuth} = useAuth()
@@ -8,7 +9,7 @@ const OrderPage = () => {
     <div className="page">
       <div className="container">
         <h1>주문내역</h1>
-        
+        <OrdersList />
       </div>
     </div>
   )
---
+++ src/pages/OrderPage/orders-list/OrdersList.jsx
@@ -0,0 +1,42 @@
+import styles from "./OrdersList.module.scss"
+import { useEffect } from "react"
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux"
+import { useAuth } from "../../../hooks/useAuth"
+import { fetchOrder } from "../../../store/order/order.slice"
+import CartEmpty from "../../../components/cart-empty/CartEmpty"
+import OrderItem from "./order-item/OrderItem"
+
+const OrdersList = () => {
+    const {id} = useAuth()
+    const {order} = useAppSelector(state => state.orderSlice)
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+      dispatch(fetchOrder(id))
+    }, [id])
+
+    if(!order.length) {
+        return <CartEmpty title="주문내역" />
+    }
+    
+
+    return (
+        <div className={styles.orders}>
+            {order.map(item => (
+                <div key={item.id}>
+                    <div className={styles.order_header}>
+                        <h3>주문번호_{item.id}</h3>
+                        <p>합계: $ {item.totalPrice.toFixed(2)}</p>
+                    </div>
+                    <ul className={styles.orders_list}>
+                        {item.products.map(order => (
+                            <OrderItem key={order.id} order={order} />
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default OrdersList
