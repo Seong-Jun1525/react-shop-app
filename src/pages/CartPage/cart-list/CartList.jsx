d6d2543173c3080d34794346c37bc58e2960093c
Seong-Jun1525 <sjsj123455@naver.com>
2023-12-06 12:01:08 +0900

    create CartList

---
+++ src/pages/CartPage/cart-list/CartList.jsx
@@ -0,0 +1,15 @@
+import styles from "./CartList.module.scss"
+import CartItem from "./cart-item/CartItem"
+
+const CartList = () => {
+    const {products} = useAppSelector((state) => state.cartSlice)
+    return (
+        <div className={styles.cart_list}>
+            {products.map((product) => {
+                <CartItem key={product.id} item={product} />
+            })}
+        </div>
+    )
+}
+
+export default CartList
